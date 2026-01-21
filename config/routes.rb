15d1a9531e754aeaa21f1d5b45c00318d6c837be
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-01-21 16:50:31 +0900

    feat: add unsubscribe link to notification emails

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :products do
     resources :subscribers, only: [ :create ]
   end
+  resource :unsubscribe, only: [ :show ]
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
