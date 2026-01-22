97199f1b0da9ef56758c7757a7ab9796e970adc7
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-01-22 16:23:33 +0900

    feat: add email address update functionality

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.email_confirmation.subject
+  #
+  def email_confirmation
+    @token = params[:user].generate_token_for(:email_confirmation)
+
+    mail to: "to@example.org"
+  end
+end
