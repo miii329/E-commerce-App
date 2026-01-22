4fa74c3fe68ec1ba08108ab8dccd9ff64309a290
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-01-22 22:35:19 +0900

    feat: separate admin and user access controls

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,10 @@ end
 namespace :email do
   resources :confirmations, param: :token, only: [ :show ]
 end
+
+namespace :store do
+  resources :users
+end
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
