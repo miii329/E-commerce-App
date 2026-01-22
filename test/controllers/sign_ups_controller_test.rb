5d3c520365ba5c568538753bf89946453a8629a2
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-01-23 02:30:20 +0900

    test: サインイン済みユーザーがサインアップページにアクセスした際のリダイレクトを確認

--- test/controllers/sign_ups_controller_test.rb
+++ test/controllers/sign_ups_controller_test.rb
@@ -5,4 +5,10 @@ class SignUpsControllerTest < ActionDispatch::IntegrationTest
     get sign_up_path
     assert_response :success
   end
+
+  test "view sign up when authenticated" do
+  sign_in_as users(:one)
+  get sign_up_path
+  assert_redirected_to root_path
+end
 end
