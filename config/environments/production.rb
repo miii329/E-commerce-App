9f04fab549bd1e9555a8864a2e840f8cd5403663
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-01-26 23:00:00 +0900

    fix: RuboCop違反を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   # Store uploaded files with Cloudinary in production
   config.active_storage.service = :cloudinary
-  
+
   # Active Storageのルーティングを有効にする
   config.active_storage.resolve_model_to_route = :rails_storage_proxy
 
